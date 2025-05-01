2e6a52ef01b1f22198ec8c8a4ea153a69d589149
Boris <bgrebennikovv@gmail.com>
2025-05-01 17:51:59 +0400

    make user phone as basic login field

--- src/main/kotlin/com/example/sample/repository/UserRepository.kt
+++ src/main/kotlin/com/example/sample/repository/UserRepository.kt
@@ -2,9 +2,8 @@ package com.example.sample.repository
 
 import com.example.sample.entity.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 interface UserRepository : JpaRepository<UserEntity, Int> {
-    fun findByUsername(username: String): UserEntity?
-
-    fun findByUsernameOrEmail(username: String, email: String): UserEntity?
+    fun findByPhone(phone: String): UserEntity?
 }
